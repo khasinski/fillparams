ca8b9d205a0d64076385246ce363678e38c24c43
justi <justine84@gmail.com>
2014-07-01 23:01:13 +0200

    Load init.yml file

--- lib/config_manager.rb
+++ lib/config_manager.rb
@@ -2,12 +2,14 @@
 require 'konf'
 
 class ConfigManager
-  def initialize
-    @pub_config = {}
+  attr_reader :init_files
+
+  def initialize init_yml_file
+    @init_files = load init_yml_file
   end
 
   def load yml_file
-    @pub_config = YAML.load(ERB.new(File.read(File.expand_path("../../#{yml_file}", __FILE__))).result)
+    YAML.load(ERB.new(File.read(File.expand_path("../../#{yml_file}", __FILE__))).result)
   end
 end
 
