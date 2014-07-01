c3dec245b3dab6e5dbf09cd3d6005a1b34263ca9
justi <justine84@gmail.com>
2014-07-01 23:57:18 +0200

    Add rspec

---
+++ spec/foobar_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+describe 'Foobar' do
+  pending "write it"
+  it 'should be true' do
+    expect(1).to eq 1
+  end
+end
+
---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+require 'bundler/setup'
+
+Bundler.setup
+
+RSpec.configure do |config|
+end
