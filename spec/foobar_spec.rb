9b5d5c736e351052eaa0e1bea0fff9f968be05f8
justi <justine84@gmail.com>
2014-07-04 23:56:59 +0200

    ParametrFiller test

--- spec/foobar_spec.rb
+++ spec/foobar_spec.rb
@@ -1,8 +1,18 @@
 require 'spec_helper'
 describe ParameterFiller do
-  pending "write it"
-  it 'should be true' do
-    expect(1).to eq 1
+
+  def sample_dist_data
+    YAML.load(<<-EOF
+      NAME: ABC
+    EOF
+    )
+  end
+
+  let(:pf) { ParameterFiller.new('', '', '') }
+  context 'when the data is the same as sample data' do
+    let(:data){ sample_dist_data }
+    it 'returns the same config' do
+      expect(pf.fill_parameters(data, sample_dist_data)).to eq sample_dist_data
+    end
   end
 end
-
